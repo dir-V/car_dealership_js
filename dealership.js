e59808b6c6d101ae58910fd69f13b5d2d9f49878
Vincent Adeola <vince.adeola@outlook.com>
2023-05-04 19:11:16 +0100

    dealership class fixed

--- dealership.js
+++ dealership.js
@@ -14,16 +14,36 @@ Dealership.prototype.countCars = function(){
     console.log(stocks.length);
 };
 
-Dealership.prototype.filterManu = function(manufacturers){
-       const filtered = stocks.filter(stocks.Car.manufacturer.includes(manufacturers));
+Dealership.prototype.getAllManu = function(){
+    const manufacturers= stocks.map((car)=> {
+        return car.manufacturer;
+    })
+    console.log(manufacturers);
+}
+
+Dealership.prototype.filterManu = function(manufacturer){
+       const filteredItems = stocks.filter(a => {
+        return a.manufacturer === manufacturer;
+       })
+       console.log(filteredItems)
     
 };
 
+Dealership.prototype.AddUpStock = function(){
+   const total = stocks.reduce((accumulator, car) => {
+    return accumulator + car.price;
+    }, 0);
+   console.log(total);
+}
+
 const v = new Dealership("V");
-const Vcar = new Car ("Vdealer", 100, "V88");
+const Vcar = new Car ("Toyota", 100, "V88");
 
 v.addCar(Vcar);
-console.log(Vcar);
+// console.log(Vcar);
+v.getAllManu();
 v.countCars();
-v.filterManu("Vdealer");
-console.log();
+v.filterManu("Toyota");
+v.AddUpStock();
+// console.log();
+// v.filterManu(2);
